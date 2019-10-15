1da66712497fb2a5a1067778ca4e74092bf79918
Nolan Kovacik <noltron000@gmail.com>
2019-10-15 13:37:13 -0700

    [fmt] file extentions in imports

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// this option is a personal convention //
+// ? how to set this up in tsconfig ? //
+// @ts-ignore //
+import App from './App.tsx';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
