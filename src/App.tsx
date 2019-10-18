6ca4940854b33de9a1245dc71c3dee1d994d046d
Nolan Kovacik <noltron000@gmail.com>
2019-10-18 11:05:22 -0700

    [add] splitting boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
 import React from 'react'
-import Typography from '@material-ui/core/Typography'
-import logo from './logo.svg'
-import './App.scss'
+// import './App.scss'
+// this @ts-ignore option is a personal convention
+// ? how to set this up in tsconfig ?
+// @ts-ignore
+import Boilerplate from './Boilerplate.tsx'
 
 const App: React.FC = () => (
-	<div className='App'>
-		<header className='App-header'>
-			<Typography variant='h1'>
-				Hello World
-			</Typography>
-			<img
-				src={logo}
-				className='App-logo'
-				alt='logo'
-			/>
-			<p>
-				Edit <code>src/App.tsx</code> and save to reload.
-			</p>
-			<a
-				className='App-link'
-				href='https://reactjs.org'
-				target='_blank'
-				rel='noopener noreferrer'
-			>
-				Learn React
-			</a>
-		</header>
-	</div>
+	<>
+		<Boilerplate />
+	</>
 )
 
 export default App
