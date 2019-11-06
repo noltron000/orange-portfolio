3817cf4e581c564c72fb9391e562e524800270e2
Nolan <noltron000@gmail.com>
2019-11-05 20:46:36 -0800

    [upd] story layout

--- src/Story.tsx
+++ src/Story.tsx
@@ -3,23 +3,33 @@ import './Story.scss'
 
 const Story: React.FC = () => (
 	<article className='post'>
-		<time dateTime='10-21-2019'>October 21st, 2019</time>
+		<time
+			className='date-published'
+			dateTime='10-21-2019'
+		>
+			October 21st, 2019
+		</time>
 		<a href='#nothing'>
-			<h2>
-				Story Header
-			</h2>
+			<h1>Story Header</h1>
+		</a>
+		<a href='#nothing'>
 			<picture>
 				<source
-					srcSet='https://placekitten.com/1200/600'
+					srcSet='https://placekitten.com/1024/512'
 					media='(min-width: 800px)'
 				/>
 				<img
-					src='https://placekitten.com/400/600'
+					src='https://placekitten.com/512/320'
 					alt='placeholder kitten'
 				/>
 			</picture>
 		</a>
-		<p>Lorem Ipsum Fodarum. Joilinaski asdif disinti vololox.</p>
+		<p>
+			Aenean ornare velit lacus varius enim ullamcorper proin,
+			aliquam facilisis ante sed etiam magna interdum congue.
+			Lorem ipsum dolor amet nullam sed etiam veroeros.
+			Joilinaski asdif disinti vololox.
+		</p>
 		<a href='#nothing'>
 			Full Story
 		</a>
